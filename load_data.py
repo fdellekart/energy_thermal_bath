01dea8e1982a6647cb17acd20e81cf3d27d3b2cc
Florian Dellekart <florian.dellekart@hotmail.de>
2020-01-18 20:52:59 +0100

    Added function to turn time string from csv into datetime object

--- load_data.py
+++ load_data.py
@@ -1,6 +1,22 @@
 import pandas as pd
+from datetime import datetime
 
 with open("load_curve_thermal_bath.csv", 'r') as f:
     data = pd.read_csv(f)
 
-print(data)
+#print(data)
+
+#print(data.loc[0,'times'])
+
+
+def get_date_time(datetime_string):
+    day_int = int(datetime_string[0:2])
+    month_int = int(datetime_string[3:5])
+    year_int = int(datetime_string[6:10])
+    hour_int = int(datetime_string[11:13])
+    minute_int = int(datetime_string[14:16])
+    second_int = int(datetime_string[17:19])
+
+    return datetime(year_int, month_int, day_int, hour_int, minute_int, second_int)
+
+print(get_date_time(data.loc[0,'times']))    
