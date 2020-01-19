3d35ecb6a54572cdf04fa84f64409b31b29391e6
Florian Dellekart <florian.dellekart@hotmail.de>
2020-01-19 18:40:42 +0100

    plot out sauna and gesamt in gridplot above eachother

--- bokeh_tryout.py
+++ bokeh_tryout.py
@@ -1,5 +1,6 @@
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import ColumnDataSource
+from bokeh.layouts import gridplot
 from thermal_bath import LoadCurve
 from datetime import datetime
 from pandas import Timestamp
@@ -9,39 +10,47 @@ DATA_SOURCE = "load_curve_thermal_bath.csv"
 load_curve = LoadCurve()
 load_curve.load_data(DATA_SOURCE)
 load_curve.data_times_to_timestamp_index("times")
+load_curve.to_kilowatts()
 
 source = ColumnDataSource(load_curve.data)
 
 output_file("load_curve.html")
 
-p = figure(title="Bokeh TryOut by Flo",
-    x_axis_label="Time",
-    y_axis_label="Load",
+sauna = figure(title="Sauna",
+    x_axis_label="Date",
+    y_axis_label="Load [kW]",
     x_axis_type="datetime",
     plot_width=1000,
-    plot_height=300,
-    tools="pan,crosshair,wheel_zoom,box_zoom,reset,box_select,lasso_select,save")
+    plot_height=300,)
 
-p.line(x="times",
+sauna.line(x="times",
     y="sauna",
-    legend_label="Sauna",
-    line_width=1,
-    source=source)
-
-p.circle(x="times",
-    y="gesamt",
-    legend_label="Gesamt",
-    fill_color="white",
     line_color="red",
     line_width=1,
     source=source)
 
-p.line(x="times",
+# p.circle(x="times",
+#     y="gesamt",
+#     legend_label="Gesamt",
+#     fill_color="white",
+#     line_color="red",
+#     line_width=1,
+#     source=source)
+
+gesamt = figure(title="Gesamt",
+    x_axis_label="Date",
+    y_axis_label= "Load [kW]",
+    x_axis_type="datetime",
+    plot_width=1000,
+    plot_height=300) 
+
+gesamt.line(x="times",
     y="gesamt",
-    legend_label="Gesamt",
-    line_color="red",
+    line_color="black",
     line_width=1,
     source=source)
+
+p = gridplot([[sauna],[gesamt]], toolbar_location=None)
 
 
 show(p)
