b1e5e6cac95c21a4a8205c29bfc54c7790b4df8f
Florian Dellekart <florian.dellekart@hotmail.de>
2020-01-20 14:08:07 +0100

    removed to_kilowatts method and replaced with set_unit
    Setting all values in self.data to W, kW, MW, GW or TW is now possible
    
    times_to_index must run first to avoid trying to turn Timestamps into
    units.

--- thermal_bath.py
+++ thermal_bath.py
@@ -7,11 +7,13 @@ from datetime import datetime
 class LoadCurve:
     def __init__(self):
         self.data = None
+        self.unit = None
 
 
-    def load_data(self, src_path):
+    def load_data(self, src_path, unit):
         """loads data from csv at src_path
         puts it into self.data as pd.DataFrame"""
+        self.unit = unit
         with open(src_path, 'r') as f:
             self.data = pd.read_csv(f)
 
@@ -36,10 +38,19 @@ class LoadCurve:
 
     def times_to_index(self,time_key):
         """time_key: key of collumn that holds Timestamps
-        sets index "time_key" column to index"""
+        sets time_key column to index"""
         self.data.set_index(time_key, inplace=True)
 
-    def to_kilowatts(self):
-        for key in self.data:
-            self.data[key].apply(lambda x: x/1000)
-        
+
+    def set_unit(self, unit):
+        """unit: 'W', 'kW', 'MW', 'GW' or 'TW'
+        turns all values in self.data to parsed unit
+        sets self.unit to parsed unit
+        set times_to_index first"""
+        factor_dict = {'W' : 10**0, 'kW' : 10**3, 'MW' : 10**6, 'GW': 10**9, 'TW' : 10**12}
+        if unit in factor_dict.keys():
+            for key in self.data:
+               self.data[key] = self.data[key].apply(lambda x: (x * factor_dict[self.unit]) / factor_dict[unit])
+            self.unit = unit
+        else:
+            raise Exception("KeyError: Unit not existing")
