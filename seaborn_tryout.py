a1f368a748ed798ccf90c9f4d7787d901bcb948b
Florian Dellekart <florian.dellekart@hotmail.de>
2020-01-20 17:47:58 +0100

    loaded LoadCurve from thermal_bath, plotted seaborn pairplot of data

---
+++ seaborn_tryout.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+from thermal_bath import LoadCurve
+import matplotlib.pyplot as plt
+
+TIME_KEY = "times"
+SRC_PATH = "load_curve_thermal_bath.csv"
+
+sns.set()
+
+load_curve = LoadCurve()
+load_curve.load_data(SRC_PATH, "W")
+load_curve.time_to_datetime(TIME_KEY)
+load_curve.time_to_index(TIME_KEY)
+load_curve.set_unit("kW")
+
+sns.pairplot(load_curve.get_data())
+
+plt.show()
